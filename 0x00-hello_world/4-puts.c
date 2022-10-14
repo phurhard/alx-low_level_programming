b7aff163f4e6ac1413859e3628c4c59c2aca0e24
phurhard <phurhardeen@gmail.com>
2022-10-13 21:25:23 -0700

    Resolved again

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-/* A c program to print to the standard output using the puts*/ 
+/*A c program to print to the standard output using the puts*/ 
 /**
  * main - Entry point
  *
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle");
-	return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
