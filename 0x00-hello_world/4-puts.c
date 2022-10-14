662d29f6e699338faecda7ceb40c12d77f30ef15
phurhard <phurhardeen@gmail.com>
2022-10-13 21:23:03 -0700

    resoled issues with task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 /* A c program to print to the standard output using the puts*/ 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts ("\"Programming is like building a multilingual puzzle");
 	return(0);
 }
