6a7f2dd3c070066e2f374a67ce9634ae4ed1a2e1
phurhard <phurhardeen@gmail.com>
2023-01-20 03:49:17 +0100

    Redid 100-operations

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+/**
+ * add - Adds two integers and prints the result
+ *@a: user first input
+ *@b: user second input
+ * Return: Returns the value of addition
+ *
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - Subtracts two integers and prints the result
+ * @a: user first input
+ * @b: user second input
+ * Return: Returns the value of subtraction
+ *
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * div - Divides two integers and prints the result
+ * @a: user first input
+ * @b: user second input
+ * Return: Returns the value of division
+ *
+ */
+int div(int a, int b)
+{
+        return (a / b);
+}
+/**
+ * mul - Subtracts two integers and prints the result
+ * @a: user first input
+ * @b: user second input
+ * Return: Returns the value of multiplication
+ *
+ */
+int mul(int a, int b)
+{
+        return (a * b);
+}
+/**
+ * mod - Subtracts two integers and prints the result
+ * @a: user first input
+ * @b: user second input
+ * Return: Returns the modulus of the operation
+ *
+ */
+int mod(int a, int b)
+{
+        return (a % b);
+}
+
--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,7 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
-
 int add(int a, int b);
 int mod(int a, int b);
 int sub(int a, int b);
