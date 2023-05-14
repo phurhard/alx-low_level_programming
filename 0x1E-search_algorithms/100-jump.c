86c8acff11c9954f45a7aca000c1d82f2a8222e3
phurhard <phurhardeen@gmail.com>
2023-05-14 14:36:08 +0100

    tired of jump searching

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -16,35 +16,55 @@ int jump_search(int *array, size_t size, int value)
 	int prev = 0;
 	int Size = (int)size;
 	int step = sqrt(size);
+	int place;
 
 	while (array[min(Size, step) - 1] < value)
 	{
-		printf("Value checked array[%d] = %d\n", prev, array[prev]);
+		printf("Beggining: Value checked array[%d] = [%d]\n", prev, array[prev]);
+		place = prev;
 		prev = step;
 		step += sqrt(Size);
 		if (prev >= Size)
+		{
+			printf("000: Value found between indexes [%d] and [%d]\n", place, step);
 			return (-1);
+		}
+		printf("1111: Value found between indexes %d and %d\n", place, prev);
 	}
-	printf("Value found between indexes %d and %d\n", prev, step);
-
 	while (array[prev] < value)
 	{
-		printf("Value checked array[%d] = %d\n", prev, array[prev]);
+		printf("Value checked second array[%d] = %d\n", prev, array[prev]);
 		prev += 1;
 		if (prev == min(step, Size))
+		{
+			printf("If STATEMET: Value found between indexes [%d] and [%d]\n", prev, step);
 			return (-1);
+		}
 	}
 
 	if (array[prev] == value)
+	{
+		printf("If statement: Value checked array[%d] = [%d]\n", prev, array[prev]);
 		return (prev);
+	}
 	else
+	{
+		printf("Else statement: Value found between indexes [%d] and [%d]\n", prev, step);
 		return (-1);
+	}
 }
+
+/**
+ * min - Finds the minimum between two input values
+ * @a: Value 1
+ * @b: Value 2
+ * Return: The smallest of the two values
+ */
 
 int min(int a, int b)
 {
 	if (a > b)
-		return b;
+		return (b);
 	else
-		return a;
+		return (a);
 }
